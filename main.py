5549d635be34babd8833b61038285c5d1c8131ce
koreahong <koreahong2@naver.com>
2021-10-10 23:56:46 +0900

    update content 211010

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import re
 import datetime
-import time
-
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,10 +10,10 @@ from bs4 import BeautifulSoup
 import requests
 
 
-stock = pd.read_csv("C:/Users/korea/OneDrive/바탕 화면/exefile/stock.csv")
+stock = pd.read_csv("./stock.csv")
 target = stock.name
 
-path = 'C:/Users/korea/OneDrive/바탕 화면/exefile/chromedriver.exe'
+path = './chromedriver_ver_94.exe'
 driver = webdriver.Chrome(path)
 
 digikey_result = []
@@ -57,65 +56,29 @@ for product_name in target:
 
 digikey_result = pd.DataFrame(digikey_result, columns=['name', 'digikey_stock'])
 
-digikey_result.to_csv('./stock_crawling_' + str(datetime.date.today()) + '.csv')
+digikey_result.to_csv('./stock_crawling_' + str(datetime.date.today()) + '.csv', index_label='no')
+
 driver.close()
 
+class API:
+  def __init__(self):
+    pass
 
-### 마우저 검색
-#
-# mouser_result = []
-#
-# driver.get("https://kr.mouser.com/")
-# wait = WebDriverWait(driver, 5)
-# time.sleep(10)
-#
-# for product_name in target:
-#     search_path = '/html/body/header/div[4]/div/div/div[2]/div/form/div/div/div[1]/div[1]/div[2]/input[1]'
-#     element = wait.until(EC.visibility_of_element_located((By.XPATH, search_path)))
-#     element.send_keys(product_name)  #검색어입력하게 하는 부분
-#     element.send_keys("\n")  #엔터효과
-#
-#
-#     ### 한번에 검색되는 경우
-#     try:
-#         product_stock_xpath = '//*[@id="pdpPricingAvailability"]/div[1]/h2"]'
-#         stock_element = wait.until(EC.visibility_of_element_located((By.XPATH, product_stock_xpath)))
-#         mouser_result.append([product_name, re.sub('[^0-9]','', stock_element.text)])
-#     except:
-#         ### 검색이 안되는 경우
-#         try:
-#             alert_xpath = '/html/body/main/div/div/div[1]/div[3]'
-#             stock_element = wait.until(EC.visibility_of_element_located((By.XPATH, alert_xpath)))
-#             mouser_result.append([product_name, '이름이 잘못됬거나 검색이 안됨'])
-#         except:
-#             ### 회사 이름이 나오는 경우
-#             try:
-#                 display_found_xpath = '//*[@id="leafCat_1"]'
-#                 element = wait.until(EC.visibility_of_element_located((By.XPATH, display_found_xpath)))
-#                 element.click()
-#             except:
-#                 pass
-#
-#             ### 상품이 검색되는 경우우
-#             try:
-#                 click_product_xpath = '//*[@id="lnkMfrPartNumber_1"]'
-#                 element = wait.until(EC.visibility_of_element_located((By.XPATH, click_product_xpath)))
-#                 element.click()
-#             except:
-#                 pass
-#
-#             ### 재고가 있는 경우
-#             try:
-#                 product_stock_xpath = '//*[@id="pdpPricingAvailability"]/div[1]/h2"]'
-#                 stock_element = wait.until(EC.visibility_of_element_located((By.XPATH, product_stock_xpath)))
-#                 mouser_result.append([product_name, re.sub('[^0-9]', '', stock_element.text)])
-#
-#             ### 재고가 없는 경우
-#             except:
-#                 product_stock_xpath = '//*[@id="InStockNotify"]'
-#                 stock_element = wait.until(EC.visibility_of_element_located((By.XPATH, product_stock_xpath)))
-#                 mouser_result.append([product_name, 0])
-#
-#
-#
-# mouser_result = pd.DataFrame(mouser_result, columns=['name', 'mouser_stock'])
+  def get_mouser(self, product_key):
+    headers = {
+      'accept': 'text/json',
+      'Content-Type': 'text/json',
+    }
+
+    params = (
+        ('apiKey', '20ab72a1-96c2-42a7-8feb-998ee9a184f1'),
+    )
+
+    data = '{"SearchByKeywordRequest": { "keyword": "' + str(product_key) + '", "records": 0, "startingRecord": 0, "searchOptions": "string", "searchWithYourSignUpLanguage": "string" }}'
+
+    response = requests.post('https://api.mouser.com/api/v1.0/search/keyword', headers=headers, params=params, data=data)
+
+    return int(re.sub('[^0-9]', '', response.json()['SearchResults']['Parts'][0]['Availability']))
+
+API = API()
+print(API.get_mouser('ZMOD4510AI1R'))
